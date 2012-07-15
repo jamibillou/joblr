53e53b0d8fb229dc98f70b2ac7cc3fce75983aaa
Dominic Matheron <d.matheron@gmail.com>
2012-07-15 14:50:23 +0200

    Created a pages controller and a home view

--- config/routes.rb
+++ config/routes.rb
@@ -46,13 +46,5 @@ Joblr::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  root :to => 'pages#home'
 end
