55b3d557c8e973969cb74e4d02c8b7b271c65952
Dominic Matheron <d.matheron@gmail.com>
2012-07-15 12:40:22 +0200

    Created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joblr::Application.config.secret_token = 'dcdd096f9cd30ddf89b7944fc6fd21a5c2df8bc0c81e6850cd6a0b47caa2e5e11232ccaa76d87823008536e78cde6138f4b20e04ab5dbc0d0d8e1240a9311db5'
