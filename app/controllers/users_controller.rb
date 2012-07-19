8f179581edbea8da1a917aadcd6a69411fbc7c02
Dominic Matheron <d.matheron@gmail.com>
2012-07-20 01:18:26 +0200

    Updated users edit view, update action now working yet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,13 @@ class UsersController < ApplicationController
   	@user = User.find params[:id]
   	@user.profiles.build
   end
+
+  def update
+    @user = User.find params[:id]
+    unless @user.update_attributes params[:user]
+      flash[:error] = 'Houston we have a problem!'
+    else
+      redirect_to @user, success: 'You have a profile man!'
+    end
+  end
 end
