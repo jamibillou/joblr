767e1c513e5f9ecf97ed4b945387657fc850202a
Dominic Matheron <dominic@soocial.com>
2012-07-24 10:23:29 +0200

    Added flash messages to users update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  include ApplicationHelper
+
   def show
     @user = params[:id] ? User.find(params[:id]) : current_user
     redirect_to edit_user_path(@user) if @user.profiles.empty?
@@ -13,9 +15,9 @@ class UsersController < ApplicationController
   def update
     @user = User.find params[:id]
     unless @user.update_attributes params[:user]
-      redirect_to edit_user_path(@user), error: 'Houston we have a problem!'
+      redirect_to edit_user_path(@user), flash: { error: error_messages(@user) }
     else
-      redirect_to @user, success: 'You have a profile man!'
+      redirect_to @user, flash: { success: t('flash.success.profile_updated') }
     end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def error_messages(object, options = {})
+    errors = unduplicated_errors(object, options).map! do |attribute, message|
+      "#{object.class.human_attribute_name(attribute).downcase} #{message}"
+    end.to_sentence
+    "#{t('flash.error.base')} #{errors}."
+  end
+
+  def unduplicated_errors(object, options = {})
+    object.errors.select do |attribute, message|
+      options[:only] ? options[:only].include?(attribute) && message == object.errors[attribute].first : message == object.errors[attribute].first
+    end
+  end
 end
