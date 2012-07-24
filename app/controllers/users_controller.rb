f9b4c12438ee417d3eac788d294c5d131a51d513
Dominic Matheron <d.matheron@gmail.com>
2012-07-24 23:17:30 +0200

    Working delete current file check_box

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,19 @@ class UsersController < ApplicationController
     unless @user.update_attributes params[:user]
       redirect_to edit_user_path(@user), flash: { error: error_messages(@user) }
     else
+      remove_file! @user.profiles.first if remove_file?(params)
       redirect_to @user, flash: { success: t('flash.success.profile_updated') }
     end
   end
+
+  private
+
+    def remove_file?(params)
+      params[:user][:profiles_attributes]['0'][:remove_file].to_i == 1
+    end
+
+    def remove_file!(profile)
+      profile.remove_file!
+      profile.update_attributes file: nil
+    end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,6 @@
 class Profile < ActiveRecord::Base
 
-  attr_accessible :education, :experience, :quality_1, :quality_2, :quality_3, :skill_1, :skill_1_level, :skill_2, :skill_2_level, :skill_3, :skill_3_level, :text, :url, :file, :user_id
+  attr_accessible :education, :experience, :quality_1, :quality_2, :quality_3, :skill_1, :skill_1_level, :skill_2, :skill_2_level, :skill_3, :skill_3_level, :text, :url, :file, :remove_file, :user_id
 
   belongs_to :user
 
