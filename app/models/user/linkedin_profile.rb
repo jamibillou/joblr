ec0f8e5ba870230836290092ba462af986cb3614
Dominic Matheron <dominic@soocial.com>
2012-08-01 00:10:53 +0200

    Working LinkedIn import
    Images not taken into account yet, quite freaking messy for now

--- app/models/user/linkedin_profile.rb
+++ app/models/user/linkedin_profile.rb
@@ -2,19 +2,17 @@ module User::LinkedinProfile
 
   def linkedin_profile
     raw_hash = linkedin_client.profile(fields: %w(first_name last_name location positions educations skills))
-    clean_hash = {
-      firstname:       raw_hash.first_name,
-      lastname:        raw_hash.last_name,
-      city:            raw_hash.location.name.split(',').first.gsub(' Area',''),
-      country:         raw_hash.location.name.split(',').last,
-      current_role:    raw_hash.positions.all.select(&:is_current?).first.title,
-      current_company: raw_hash.positions.all.select(&:is_current?).first.company.name,
-      experience:      experience_duration(raw_hash.positions.all),
-      experiences:     raw_hash.positions.all.map { |exp| { role: exp.title, company: exp.company.name, current: exp.is_current, desc: exp.summary, start_date: ({ month: exp.start_date.month, year: exp.start_date.year } unless exp.start_date.nil?), end_date: ({ month: exp.end_date.month, year: exp.end_date.year } unless exp.end_date.nil?) } },
-      educations:      raw_hash.educations.all.map{ |edu| { degree: edu.degree, field: edu.field_of_study, school: edu.school_name, start_date: ({ month: edu.start_date.month, year: edu.start_date.year } unless edu.start_date.nil?), end_date: ({ month: edu.end_date.month, year: edu.end_date.year } unless edu.end_date.nil?) } },
-      skills:          raw_hash.skills.all.map { |sk| sk.skill.name }
-    }
-    clean_hash
+
+    { first_name:       raw_hash.first_name,
+      last_name:        raw_hash.last_name,
+      city:             raw_hash.location.name.split(',').first.gsub(' Area',''),
+      country:          raw_hash.location.name.split(',').last,
+      current_role:     raw_hash.positions.all.select(&:is_current?).first.title,
+      current_company:  raw_hash.positions.all.select(&:is_current?).first.company.name,
+      experience:       experience_duration(raw_hash.positions.all),
+      education_degree: raw_hash.educations.all.first.degree,
+      education_field:  raw_hash.educations.all.first.field_of_study,
+      skills:           raw_hash.skills.all.map { |sk| sk.skill.name } }
   end
 
   def linkedin_client
@@ -26,13 +24,18 @@ module User::LinkedinProfile
 
   def experience_duration(positions)
     unless positions.nil?
+      # fixes nil end_date on current position
+      if positions.first.end_date.nil? && positions.first.is_current
+        positions.first.end_date = {month: Time.now.month, year: Time.now.year}
+      end
+
+      # calculates the different between the first and the last positions
       unless positions.first.end_date.nil? || positions.last.start_date.nil?
         duration = positions.first.end_date.year - positions.last.start_date.year - 1
         unless positions.first.end_date.month.nil? || positions.last.start_date.month.nil?
           duration += (13 - positions.first.end_date.month + positions.last.start_date.month) / 12.0
         end
-      end
-      duration
+      end.round
     end
   end
 end
