5607ef5f67ff1f6e37137aa52c6b6ebb00807441
Dominic Matheron <d.matheron@gmail.com>
2012-08-05 19:19:57 +0200

    Minor changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120731185048) do
+ActiveRecord::Schema.define(:version => 20120729195909) do
 
   create_table "authentifications", :force => true do |t|
     t.integer  "user_id"
@@ -19,10 +19,10 @@ ActiveRecord::Schema.define(:version => 20120731185048) do
     t.string   "uid"
     t.string   "uname"
     t.string   "uemail"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.string   "url"
-    t.string   "oauth_token"
+    t.string   "upic"
     t.string   "utoken"
     t.string   "usecret"
   end
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(:version => 20120731185048) do
     t.string   "username"
   end
 
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
   add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
