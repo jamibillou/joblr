aad9dde244ea663962ceeb513d4f47de5125dcf1
Franck Sabattier <franck.sabattier@gmail.com>
2012-08-10 19:36:46 +0200

    Try to fix pic display on emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,6 @@ Joblr::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  # Default url required for Devise
   # In production, :host should be set to the actual host of your application.
   config.action_mailer.default_url_options = { :host => "localhost", :port => "3000" }
 end
