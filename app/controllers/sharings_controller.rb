3d9ff8c9796dc7dbce2c74125c2765a0e250de1a
Dominic Matheron <d.matheron@gmail.com>
2012-08-13 09:00:31 +0200

    Minor cleanup

--- app/controllers/sharings_controller.rb
+++ app/controllers/sharings_controller.rb
@@ -27,12 +27,13 @@ class SharingsController < ApplicationController
 	end
 
 	def linkedin
-		@user = User.find(params[:sharing][:user_id])
-		unless params[:sharing][:text].blank? || params[:sharing][:text].length > 140
-      current_user.linkedin_share title: "#{@user.fullname}'s profile on Joblr", comment: params[:sharing][:text], url: root_url(subdomain: @user.subdomain), image_url: @user.image_url.to_s
+		@user = User.find params[:sharing][:user_id]
+		error = params[:sharing][:text].blank? ? t('flash.error.text.blank') : (t('flash.error.text.long') if params[:sharing][:text].length > 140)
+    unless error
+      current_user.linkedin_share title: "#{@user.fullname} on Joblr", comment: params[:sharing][:text], url: root_url(subdomain: @user.subdomain), image_url: "http://#{request.domain}#{@user.image_url.to_s}"
       redirect_to root_path, flash: { success: t('flash.success.profile_shared') }
     else
-    	redirect_to new_sharing_path(id: @user.id, provider: 'linkedin'), flash: { error: "Text format is incorrect (can't be blank or over 140 characters)" }
+    	redirect_to new_sharing_path(id: @user.id, provider: 'linkedin'), flash: { error: error }
 		end
 	end
 
