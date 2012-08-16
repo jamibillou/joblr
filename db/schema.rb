0e1a421bbba4285b0adc8cbdec5856e3cb4a621b
Dominic Matheron <dominic@soocial.com>
2012-08-16 12:43:06 +0200

    Updated .gitignore again

--- db/schema.rb
+++ db/schema.rb
@@ -19,10 +19,10 @@ ActiveRecord::Schema.define(:version => 20120806183440) do
     t.string   "uid"
     t.string   "uname"
     t.string   "uemail"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.string   "url"
-    t.string   "oauth_token"
+    t.string   "upic"
     t.string   "utoken"
     t.string   "usecret"
   end
@@ -57,8 +57,8 @@ ActiveRecord::Schema.define(:version => 20120806183440) do
 
   create_table "users", :force => true do |t|
     t.string   "fullname"
-    t.string   "email",                  :null => true
-    t.string   "encrypted_password",     :null => true
+    t.string   "email"
+    t.string   "encrypted_password"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -67,8 +67,8 @@ ActiveRecord::Schema.define(:version => 20120806183440) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",             :null => false
-    t.datetime "updated_at",             :null => false
+    t.datetime "created_at",                            :null => false
+    t.datetime "updated_at",                            :null => false
     t.string   "city"
     t.string   "country"
     t.string   "role"
