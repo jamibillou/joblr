bdeda9e8dc014ed8889148cbbabac3491c431958
Dominic Matheron <d.matheron@gmail.com>
2012-09-10 23:12:28 +0200

    Minor fixes

--- app/controllers/email_sharings_controller.rb
+++ app/controllers/email_sharings_controller.rb
@@ -6,21 +6,21 @@ class EmailSharingsController < ApplicationController
     unless @email_sharing.save
       respond_to {|format| format.html { render :json => t('flash.error.required.all'), :status => :unprocessable_entity if request.xhr? }}
     else
-      respond_to {|format| format.html { send(@email_sharing, @user) } }
+      respond_to {|format| format.html { send } }
     end
 	end
 
   private
 
-    def send(email_sharing, user)
+    def send
       if user_signed_in?
-        if current_user == user
-          EmailSharingMailer.user(email_sharing, user).deliver
+        if current_user == @user
+          EmailSharingMailer.user(@email_sharing, @user).deliver
         else
-          EmailSharingMailer.other_user(email_sharing, user).deliver
+          EmailSharingMailer.other_user(@email_sharing, @user, current_user).deliver
         end
       else
-        EmailSharingMailer.public_user(email_sharing, user).deliver
+        EmailSharingMailer.public_user(@email_sharing, @user).deliver
       end
       flash[:success] = t('flash.success.profile.shared', fullname: user.fullname, recipient_email: email_sharing.recipient_email)
       render :json => 'create!' if request.xhr?
