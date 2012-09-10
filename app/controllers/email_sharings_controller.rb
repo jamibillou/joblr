4099f4359ac92d507633914f608b8f33e084f8cb
Dominic Matheron <d.matheron@gmail.com>
2012-09-10 22:45:27 +0200

    Refactored EmailSharingsController so it handles public and other users

--- app/controllers/email_sharings_controller.rb
+++ app/controllers/email_sharings_controller.rb
@@ -1,20 +1,28 @@
 class EmailSharingsController < ApplicationController
 
-	before_filter :load_user, :profile_completed, only: :new
-
 	def create
 		@user = User.find params[:user_id]
-		@email_sharing = EmailSharing.new params[:email_sharing].merge(profile_id: @user.profile, author: current_user)
+		@email_sharing = EmailSharing.new params[:email_sharing].merge(profile_id: @user.profile, author: (current_user ||= nil))
     unless @email_sharing.save
       respond_to {|format| format.html { render :json => t('flash.error.required.all'), :status => :unprocessable_entity if request.xhr? }}
     else
-      respond_to do |format|
-      	format.html do
-      		EmailSharingMailer.user(@email_sharing, @user).deliver
-      		flash[:success] = t('flash.success.profile.shared')
-      	  render :json => 'create!' if request.xhr?
-      	end
-      end
+      respond_to {|format| format.html { send(@email_sharing, @user) } }
     end
 	end
+
+  private
+
+    def send(email_sharing, user)
+      if user_signed_in?
+        if current_user == user
+          EmailSharingMailer.user(email_sharing, user).deliver
+        else
+          EmailSharingMailer.other_user(email_sharing, user).deliver
+        end
+      else
+        EmailSharingMailer.public_user(email_sharing, user).deliver
+      end
+      flash[:success] = t('flash.success.profile.shared', fullname: user.fullname, recipient_email: email_sharing.recipient_email)
+      render :json => 'create!' if request.xhr?
+    end
 end
