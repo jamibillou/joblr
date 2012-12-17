3457f08613f61cb7135e5c7df0b0b707376a3e70
Dominic Matheron <d.matheron@gmail.com>
2012-12-17 21:35:12 +0100

    Fixed feedback_email_mailer spec

--- spec/mailers/feedback_email_mailer_spec.rb
+++ spec/mailers/feedback_email_mailer_spec.rb
@@ -7,26 +7,28 @@ describe FeedbackEmailMailer do
     let (:feedback_email) { FactoryGirl.create :feedback_email }
     let (:mail)           { FeedbackEmailMailer.footer_form(feedback_email) }
 
-    it 'should send the email with correct subject, author and recipient' do
-      mail.subject.should == I18n.t('mailers.feedback_email.footer_form.subject')
+    it 'should send the email with the correct subject, author and recipient' do
+      mail.subject.should == I18n.t('mailers.feedback_email.footer_form.subject', fullname: feedback_email.author.fullname)
       mail.to.should      == ['team@joblr.co']
       mail.from.should    == ['postman@joblr.co']
+      mail.reply_to.should include feedback_email.author.email
     end
 
-    it 'should have a title' do
-      mail.body.encoded.should match(I18n.t('mailers.feedback_email.footer_form.title'))
+    it 'should send the email with the correct title' do
+      mail.body.encoded.should include I18n.t('mailers.feedback_email.footer_form.title', fullname: feedback_email.author.fullname)
     end
 
-    it 'should have a feedback' do
-      mail.body.encoded.should match(I18n.t('mailers.feedback_email.footer_form.feedback'))
+    it 'should send the email with a feedback' do
+      mail.body.encoded.should include "<b>#{I18n.t('mailers.feedback_email.footer_form.feedback')}</b>"
     end
 
-    it 'should have a content' do
-      mail.body.encoded.should match(Regexp.new(I18n.t('mailers.feedback_email.footer_form.content_html')))
+    it 'should send the email with the correct content' do
+      mail.body.encoded.should include I18n.t('mailers.feedback_email.footer_form.content_html', fullname: feedback_email.author.fullname, page: feedback_email.page)
     end
 
-    it 'should have a Contact button' do
-      mail.body.encoded.should match(I18n.t('mailers.feedback_email.footer_form.button'))
+    it 'should send the email with the correct Contact button' do
+      mail.body.encoded.should include "#{I18n.t('mailers.feedback_email.footer_form.button')}"
+      mail.body.encoded.should include "mailto:#{feedback_email.author.email}?subject=RE: #{I18n.t('mailers.feedback_email.footer_form.subject', fullname: feedback_email.author.fullname)}".gsub!(' ','%20')
     end
   end
 end
