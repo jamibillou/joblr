38bfc5ab64ce07bdba353a7382d9897431fa3199
jamibillou <franck.sabattier@gmail.com>
2013-01-03 14:37:28 +0100

    Removed useless migrations

--- db/migrate/20121216134135_drop_sharings.rb
+++
@@ -1,5 +0,0 @@
-class DropSharings < ActiveRecord::Migration
-  def up
-    drop_table :sharings
-  end
-end
--- db/migrate/20130103124852_drop_emails_sharings.rb
+++
@@ -1,5 +0,0 @@
-class DropEmailsSharings < ActiveRecord::Migration
-	def up
-		drop_table :email_sharings
-	end
-end
