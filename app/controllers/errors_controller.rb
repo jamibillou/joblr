bebd201155322c14001a64e3d140d5821f6fc880
jamibillou <franck.sabattier@gmail.com>
2013-01-07 11:39:18 +0100

    Added other honeybadger calls

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -5,8 +5,10 @@ class ErrorsController < ApplicationController
   end
 
   def error_422
+  	notify_honeybadger(env["action_dispatch.exception"])
   end
 
   def error_500
+  	notify_honeybadger(env["action_dispatch.exception"])
   end
 end
