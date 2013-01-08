aa87c13a4ba018a10bba4bfdd4ba32794ac230bf
Dominic Matheron <d.matheron@gmail.com>
2013-01-08 01:19:16 +0100

    Finished admin dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,8 +7,12 @@ class PagesController < ApplicationController
   end
 
   def admin
-    @users         = User.all
-    @invite_emails = InviteEmail.all
+    @users            = User.all(order: 'created_at DESC')
+    @users_count      = @users.count
+    @invite_emails    = InviteEmail.all(order: 'created_at DESC')
+    @invites_count    = @invite_emails.count
+    @profiles_count   = @users.select{|user| signed_up?(user)}.count
+    @activation_count = ProfileEmail.all.select{|pe| pe.profile.user.id == pe.author_id}.map{|pe| pe.author_id}.uniq.count
   end
 
   def style_tile
