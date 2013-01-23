76dd0cc58eb12be269ac0c807729dcbfbb1945a3
jamibillou <franck.sabattier@gmail.com>
2013-01-23 14:42:18 +0100

    Removed InviteEmail listing from admin view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,8 +11,6 @@ class PagesController < ApplicationController
   def admin
     @users            = User.all(order: 'created_at DESC')
     @users_count      = @users.count
-    @invite_emails    = InviteEmail.all(order: 'created_at DESC')
-    @invites_count    = @invite_emails.count
     @profiles_count   = @users.select{|user| signed_up?(user)}.count
     @activation_count = ProfileEmail.all.select{|pe| pe.profile.user.id == pe.author_id}.map{|pe| pe.author_id}.uniq.count
   end
