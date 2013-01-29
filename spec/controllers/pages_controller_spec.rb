3f8cee1aa3c670b0a8d9aea9c62ca3d68d2ebf00
Dominic Matheron <d.matheron@gmail.com>
2013-01-29 22:57:17 +0100

    Fixed typo in alt landing

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -13,40 +13,42 @@ describe PagesController do
 	describe "GET 'landing'" do
 
 		before :each do
-			get :landing
+			get :landing, landing_version: 'alt'
 		end
 
 		it { response.should be_success }
 
 		it 'should have the right content' do
-			if @version == 'old'
-				response.body.should have_content I18n.t('pages.landing.catchphrase')
-				response.body.should have_content I18n.t('pages.landing.subtitle')
-				response.body.should have_content I18n.t('pages.landing.signup')
-				response.body.should have_content I18n.t('pages.landing.benefit1_title')
-				response.body.should have_content I18n.t('pages.landing.benefit1_text')
-				response.body.should have_content I18n.t('pages.landing.benefit2_title')
-				response.body.should have_content I18n.t('pages.landing.benefit2_text')
-				response.body.should have_content I18n.t('pages.landing.benefit3_title')
-				response.body.should have_content I18n.t('pages.landing.benefit3_text_html')
-				response.body.should have_content I18n.t('pages.landing.benefit4_title')
-				response.body.should have_content I18n.t('pages.landing.benefit4_text')
-				response.body.should have_content I18n.t('pages.landing.disclaimer_html')
-			elsif @version == 'new'
-				response.body.should have_content I18n.t('pages.landing.new.title')
-				response.body.should have_content I18n.t('pages.landing.new.signup')
-				response.body.should have_selector '#modal_signup'
-				response.body.should have_selector '#modal_profile'
-				response.body.should have_selector '#modal_apply'
-				response.body.should have_selector '#modal_applications'
-			elsif @version == 'alt'
-				response.body.should have_content I18n.t('pages.landing.alt.title_html', link: I18n.t('pages.landing.alt.click'))
-				response.body.should have_content I18n.t('pages.landing.alt.signup')
-				response.body.should have_selector '#modal_signup'
-				response.body.should have_selector '#modal_profile'
-				response.body.should have_selector '#modal_apply'
-				response.body.should have_selector '#modal_applications'
-			end
+		  get :landing, landing_version: 'old'
+			response.body.should have_content I18n.t('pages.landing.old.catchphrase')
+			response.body.should have_content I18n.t('pages.landing.old.subtitle')
+			response.body.should have_content I18n.t('pages.landing.old.signup')
+			response.body.should have_content I18n.t('pages.landing.old.benefit1_title')
+			response.body.should have_content I18n.t('pages.landing.old.benefit1_text')
+			response.body.should have_content I18n.t('pages.landing.old.benefit2_title')
+			response.body.should have_content I18n.t('pages.landing.old.benefit2_text')
+			response.body.should have_content I18n.t('pages.landing.old.benefit3_title')
+			response.body.should have_content I18n.t('pages.landing.old.benefit3_text_html')
+			response.body.should have_content I18n.t('pages.landing.old.benefit4_title')
+			response.body.should have_content I18n.t('pages.landing.old.benefit4_text')
+		end
+		it 'should have the right content' do
+			get :landing, landing_version: 'new'
+			response.body.should have_content I18n.t('pages.landing.new.title')
+			response.body.should have_content I18n.t('pages.landing.new.signup')
+			response.body.should have_selector '#modal_signup'
+			response.body.should have_selector '#modal_profile'
+			response.body.should have_selector '#modal_apply'
+			response.body.should have_selector '#modal_applications'
+		end
+		it 'should have the right content' do
+			get :landing, landing_version: 'alt'
+			response.body.should have_content I18n.t('pages.landing.alt.title_html', link: I18n.t('pages.landing.alt.click'))
+			response.body.should have_content I18n.t('pages.landing.alt.signup')
+			response.body.should have_selector '#modal_signup'
+			response.body.should have_selector '#modal_profile'
+			response.body.should have_selector '#modal_apply'
+			response.body.should have_selector '#modal_applications'
 		end
 
 		it 'should have the right mixpanel event' do
